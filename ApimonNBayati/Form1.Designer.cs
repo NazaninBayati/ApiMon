31ba74e4885908b8ba86e4a6bc3836bb7f6ed74d
nazaninbayati <n.bayati20@gmail.com>
2019-08-25 15:52:16 +0430

    api monitor

---
+++ ApimonNBayati/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ApimonNBayati
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(380, 369);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "push btn to start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "choose your directory";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 442);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "directory:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "ApiMonBayati";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ApimonNBayati/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Net;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace ApimonNBayati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Process eventProc = new Process();
+            eventProc.StartInfo.FileName = "cmd.exe";
+            eventProc.StartInfo.Arguments = @"/k Auditpol /set /subcategory:""File System"" /success:enable";
+            eventProc.StartInfo.UseShellExecute = false;
+            
+            eventProc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable datatable = new DataTable();
+            datatable.Clear();
+            DataRow datarow;
+            datatable.Columns.Add("EventRecordID");
+            datatable.Columns.Add("ProcessId");
+            datatable.Columns.Add("ProcessName");
+            datatable.Columns.Add("AccessList");
+            datatable.Columns.Add("ObjectName");
+            datatable.Columns.Add("TimeCreated");
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Reset();
+            watch.Start();
+            //int count = 0;
+            // int x = 0;
+            var query = new EventLogQuery("C:\\Windows\\System\\winevt\\Logs\\Security.evtx", PathType.FilePath, "*[System/EventID=4663]");
+            using (var reader = new System.Diagnostics.Eventing.Reader.EventLogReader(query))
+            {
+
+                List<EventRecord> eventList = new List<EventRecord>();
+                for (EventRecord eventInstance = reader.ReadEvent();
+                    null != eventInstance; eventInstance = reader.ReadEvent())
+                {
+                    eventInstance.FormatDescription();
+                    if ((eventInstance.Id == 4663))
+                    {
+                        XmlDocument xmlDoc = new XmlDocument();
+                        String eventXml = eventInstance.ToXml();   //we can read Event data from xml, Like xml file reading ;)
+
+
+                        xmlDoc.LoadXml(eventXml);
+                        XmlElement root = xmlDoc.DocumentElement;
+                        XmlElement el = (XmlElement)root.SelectSingleNode("Event");
+                        xmlDoc.DocumentElement.SetAttribute("xmlns", "");
+                        xmlDoc.LoadXml(xmlDoc.InnerXml);
+
+                        datarow = datatable.NewRow();
+                        XmlNode node_EventRecordID = xmlDoc.SelectSingleNode("Event/System/EventRecordID");
+                        datarow["EventRecordID"] = node_EventRecordID.InnerText;
+
+                        XmlNodeList nodeList = (xmlDoc.SelectNodes("Event/System/TimeCreated"));
+                        foreach (XmlNode elem in nodeList)
+                        {
+                            datarow["TimeCreated"] = elem.Attributes["SystemTime"].Value;
+                        }
+
+                        XmlNode node_ProcessId = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ProcessId']");
+                        datarow["ProcessId"] = node_ProcessId.InnerText;
+
+                        XmlNode node_ProcessName = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ProcessName']");
+                        datarow["ProcessName"] = node_ProcessName.InnerText;
+
+                        XmlNode node_ObjectName = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ObjectName']");
+                        datarow["ObjectName"] = node_ObjectName.InnerText;
+
+                        XmlNode node_AccessList = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='AccessList']");
+                        if (node_AccessList.InnerText.Substring(0, 6) == "%%4423")
+                        {
+                            datarow["AccessList"] = "ReadAttributes";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%1538")
+                        {
+                            datarow["AccessList"] = "READ_CONTROL";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4416")
+                        {
+                            datarow["AccessList"] = "ReadData (or ListDirectory)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4416")
+                        {
+                            datarow["AccessList"] = "ReadData (or ListDirectory)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4417")
+                        {
+                            datarow["AccessList"] = "WriteData (or AddFile)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4418")
+                        {
+                            datarow["AccessList"] = "AppendData (or AddSubdirectory or CreatePipeInstance)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%1537")
+                        {
+                            datarow["AccessList"] = "DELETE";
+                        }
+                        else
+                        {
+                            datarow["AccessList"] = node_AccessList.InnerText;
+                        }
+
+                        datatable.Rows.InsertAt(datarow, 0);
+                    }
+                    //x = x + 1;
+                }
+            }
+            dataGridView1.DataSource = datatable;
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            FolderBrowserDialog folderdialog = new FolderBrowserDialog();
+            if (folderdialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderdialog.SelectedPath;
+                Process eventProc = new Process();
+                eventProc.StartInfo.FileName = "cmd.exe";
+                eventProc.StartInfo.Arguments = @"/k subinacl /subdirectories=directoriesonly " + textBox1.Text + " /sallowdeny=users=f && subinacl /subdirectories=directoriesonly " + textBox1.Text + " /sallowdeny=Administrators=f";
+                eventProc.StartInfo.UseShellExecute = false;
+              
+                eventProc.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DataTable datatable = new DataTable();
+            datatable.Clear();
+            DataRow datarow;
+            datatable.Columns.Add("EventRecordID");
+            datatable.Columns.Add("TimeCreated");
+            datatable.Columns.Add("ProcessId");
+            datatable.Columns.Add("ProcessName");
+            datatable.Columns.Add("ObjectName");
+            datatable.Columns.Add("AccessList");
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Reset();
+            watch.Start();
+            // int count = 0;
+            //int x = 0;
+            var query = new EventLogQuery("C:\\Windows\\System32\\winevt\\Logs\\Security.evtx", PathType.FilePath, "*[System/EventID=4663]");
+            using (var reader = new System.Diagnostics.Eventing.Reader.EventLogReader(query))
+            {
+
+                List<EventRecord> eventList = new List<EventRecord>();
+                for (EventRecord eventInstance = reader.ReadEvent();
+                    null != eventInstance; eventInstance = reader.ReadEvent())
+                {
+                    eventInstance.FormatDescription();
+                    if ((eventInstance.Id == 4663))
+                    {
+                        XmlDocument xmlDoc = new XmlDocument();
+                        String eventXml = eventInstance.ToXml();   
+
+
+                        xmlDoc.LoadXml(eventXml);
+                        XmlElement root = xmlDoc.DocumentElement;
+                        XmlElement el = (XmlElement)root.SelectSingleNode("Event");
+                        xmlDoc.DocumentElement.SetAttribute("xmlns", "");
+                        xmlDoc.LoadXml(xmlDoc.InnerXml);
+
+                        datarow = datatable.NewRow();
+                        XmlNode node_EventRecordID = xmlDoc.SelectSingleNode("Event/System/EventRecordID");
+                        datarow["EventRecordID"] = node_EventRecordID.InnerText;
+
+                        XmlNodeList nodeList = (xmlDoc.SelectNodes("Event/System/TimeCreated"));
+                        foreach (XmlNode elem in nodeList)
+                        {
+                            datarow["TimeCreated"] = elem.Attributes["SystemTime"].Value;
+                        }
+
+                        XmlNode node_ProcessId = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ProcessId']");
+                        datarow["ProcessId"] = node_ProcessId.InnerText;
+
+                        XmlNode node_ProcessName = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ProcessName']");
+                        datarow["ProcessName"] = node_ProcessName.InnerText;
+
+                        XmlNode node_ObjectName = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='ObjectName']");
+                        datarow["ObjectName"] = node_ObjectName.InnerText;
+
+                        XmlNode node_AccessList = xmlDoc.SelectSingleNode("Event/EventData/Data[@Name='AccessList']");
+                        if (node_AccessList.InnerText.Substring(0, 6) == "%%4423")
+                        {
+                            datarow["AccessList"] = "ReadAttributes";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%1538")
+                        {
+                            datarow["AccessList"] = "READ_CONTROL";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4416")
+                        {
+                            datarow["AccessList"] = "ReadData (or ListDirectory)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4416")
+                        {
+                            datarow["AccessList"] = "ReadData (or ListDirectory)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4417")
+                        {
+                            datarow["AccessList"] = "WriteData (or AddFile)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%4418")
+                        {
+                            datarow["AccessList"] = "AppendData (or AddSubdirectory or CreatePipeInstance)";
+                        }
+                        else if (node_AccessList.InnerText.Substring(0, 6) == "%%1537")
+                        {
+                            datarow["AccessList"] = "DELETE";
+                        }
+                        else
+                        {
+                            datarow["AccessList"] = node_AccessList.InnerText;
+                        }
+
+
+                        datatable.Rows.InsertAt(datarow, 0);
+                    }
+                    //x = x + 1;
+                }
+            }
+            dataGridView1.DataSource = datatable;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//nazaninbayati
